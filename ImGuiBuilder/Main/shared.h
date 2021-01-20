b69b6ab72d1dff2c6ab65ae9bf96c4631d1a163f
SmoLL-iCe <smoll.w.ice@gmail.com>
2021-01-19 22:10:43 -0300

    up

---
+++ ImGuiBuilder/Main/shared.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "imgui.h"
+#include "opengl/imgui_impl_glfw.h"
+#include "opengl/imgui_impl_opengl3.h"
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <Windows.h>
+#include <fstream>
+#include <sstream>
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include "ImGuiBuilder.h"
