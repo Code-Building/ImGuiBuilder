3d45b202f1433230d61e1a215165da477db599ab
HuhRyan <59122836+zimzika@users.noreply.github.com>
2021-05-05 23:26:40 -0300

    Add support for opengl2

--- ImGuiBuilder/header.h
+++ ImGuiBuilder/header.h
@@ -1,4 +1,9 @@
 #pragma once
+
+// ===========================
+// Windows IMPORTS
+// ===========================
+
 #include <Windows.h>
 #include <iostream>
 #include <string>
@@ -8,8 +13,39 @@
 #include <regex>
 #include <fstream>
 #include <sstream>
+
+// ===========================
+// Windows IMPORTS /
+// ===========================
+
+
+// ===========================
+// OPENGL Framework
+// ===========================
+
+#ifndef _OPENGL2
+	#include <GL/gl3w.h>
+#endif
+#include <GLFW/glfw3.h>
+
+// ===========================
+// OPENGL Framework /
+// ===========================
+
+
+// ===========================
+// IMGUI Framework
+// ===========================
+
 #include <imgui.h>
 #include <opengl/imgui_impl_glfw.h>
+
+#ifdef _OPENGL2
+#include <opengl/imgui_impl_opengl2.h>
+#else
 #include <opengl/imgui_impl_opengl3.h>
-#include <GL/gl3w.h>
-#include <GLFW/glfw3.h>
+#endif
+
+// ===========================
+// IMGUI Framework /
+// ===========================
